df3d511b448b1b445bf1b1d43864549c57592a2b
Mahendraiqbal24 <mahendraiqbal42@gmail.com>
2022-01-25 05:31:19 +0700

    ignore

--- src/middlewares/uploadProfile.js
+++ src/middlewares/uploadProfile.js
@@ -42,7 +42,7 @@ const uploadImage = multer({
             cb(null, true);
         } else {
             cb(null, false);
-            return cb(new Error("File must type of type .jpg .png "))
+            return cb(new Error("File must type of type .jpg .png .jpeg"))
         }
     },
     limits: { fileSize: 2 * 1024 * 1024 }
@@ -50,16 +50,16 @@ const uploadImage = multer({
 
 const single = uploadImage.single("image")
 
-const multerHandler = (req, res, next) => {
-    single(req, res, (err) => {
-        if(err && err.code === "LIMIT_FILE_SIZE") {
-           return res.status(400).json({msg: "File is too big"});
-        } else if (err) {
-            return res.status(400).json({msg: "File must type of type .jpg .png .jpeg"});
-        }
-        next();
-    });
-};
+// const multerHandler = (req, res, next) => {
+//     single(req, res, (err) => {
+//         if(err && err.code === "LIMIT_FILE_SIZE") {
+//            return res.status(400).json({msg: "File is too big"});
+//         } else if (err) {
+//             return res.status(400).json({msg: "File must type of type .jpg .png .jpeg"});
+//         }
+//         next();
+//     });
+// };
 
 
-module.exports = multerHandler;
+module.exports = single;
