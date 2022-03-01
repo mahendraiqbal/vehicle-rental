3332bf7c014c68da6d714d30bdb042b2adc5cc49
Mahendraiqbal24 <mahendraiqbal42@gmail.com>
2022-03-01 21:24:16 +0700

    console

--- src/middlewares/uploadVehicle.js
+++ src/middlewares/uploadVehicle.js
@@ -1,19 +1,17 @@
 const multer = require("multer");
 const path = require("path");
 
-
 const storage = multer.diskStorage({
-    destination: (req, res, cb) => {
-        // if () return;
-        cb(null, "./public/images/vehicle")
-    },
-    filename: (req, file, cb) => {
-        const format = `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-        cb(
-            null,
-            format
-        );
-    },
+  destination: (req, res, cb) => {
+    // if () return;
+    cb(null, "./public/images/vehicle");
+  },
+  filename: (req, file, cb) => {
+    const format = `${file.fieldname}-${Date.now()}${path.extname(
+      file.originalname
+    )}`;
+    cb(null, format);
+  },
 });
 
 // const uploadImage = multer ({
@@ -30,36 +28,35 @@ const storage = multer.diskStorage({
 //     }
 // });
 
-
-const uploadImage = multer({ 
-    storage,
-    fileFilter: (req, file, cb) => {
-        if (
-            file.mimetype == "image/png" || 
-            file.mimetype == "image/jpg" || 
-            file.mimetype == "image/jpeg"
-        ) {
-            cb(null, true);
-        } else {
-            cb(null, false);
-            return cb(new Error("File must type of type .jpg .png .jpeg"))
-        }
-    },
-    limits: { fileSize: 2 * 1024 * 1024 }
+const uploadImage = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    if (
+      (console.log(file.mimetype),
+      file.mimetype == "image/png" ||
+        file.mimetype == "image/jpg" ||
+        file.mimetype == "image/jpeg")
+    ) {
+      cb(null, true);
+    } else {
+      cb(null, false);
+      return cb(new Error("File must type of type .jpg .png .jpeg"));
+    }
+  },
+  // limits: { fileSize: 2 * 1024 * 1024 }
 });
 
-const multiple = uploadImage.array("image")
+const multiple = uploadImage.array("image");
 
 const uploadMultiple = (req, res, next) => {
-    multiple(req, res, (err) => {
-        if(err && err.code === "LIMIT_FILE_SIZE") {
-           return res.status(400).json({msg: "File is too big"});
-        } else if (err) {
-            return res.status(400).json({msg: "File must type .jpg .png .jpeg"});
-        }
-        next();
-    });
+  multiple(req, res, (err) => {
+    if (err && err.code === "LIMIT_FILE_SIZE") {
+      return res.status(400).json({ msg: "File is too big" });
+    } else if (err) {
+      return res.status(400).json({ msg: "File must type .jpg .png .jpeg" });
+    }
+    next();
+  });
 };
-
 
 module.exports = uploadMultiple;
