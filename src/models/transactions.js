0b5f13fcc521cdcf5f585e6b9b5d3ae6d8b41434
Mahendraiqbal24 <mahendraiqbal42@gmail.com>
2022-03-15 01:53:42 +0700

    transactions

--- src/models/transactions.js
+++ src/models/transactions.js
@@ -79,8 +79,8 @@ const getDataTransactionsById = (transactionId) => {
 from transactions
 inner join users ON transactions.user_id = users.id
 inner join vehicles ON transactions.vehicle_id = vehicles.id
-where users.id = ${transactionId}`;
-        db.query(sqlQuery, (err, result) => {
+where users.id = 6`;
+        db.query(sqlQuery,transactionId, (err, result) => {
             if (err) return reject({
                 status: 500,
                 err
